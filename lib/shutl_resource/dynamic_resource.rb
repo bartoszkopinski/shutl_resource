256b65601f4d175bb90e647dc260ceb54a3dbd28
David Rouchy <drouchy@gmail.com>
2012-07-09 17:00:20 +0100

    generic to_json impl

--- lib/shutl_resource/dynamic_resource.rb
+++ lib/shutl_resource/dynamic_resource.rb
@@ -4,5 +4,21 @@ module Shutl
     def initialize(args = {})
       args.each { |key, value| instance_variable_set "@#{key}", value }
     end
+
+    def to_json
+      {
+        :"#{prefix}" => to_hash
+      }.to_json
+    end
+
+    protected
+
+    def prefix
+      'test_resource'
+    end
+
+    def to_hash
+     instance_variables.inject({}) { |h, variable| h[variable.to_s.gsub('@','').to_sym] = instance_variable_get(variable) ; h }
+    end
   end
 end
--- spec/dynamic_resource_spec.rb
+++ spec/dynamic_resource_spec.rb
@@ -1,3 +1,4 @@
+require 'json'
 require_relative '../lib/shutl_resource/dynamic_resource'
 
 describe Shutl::DynamicResource do
@@ -6,19 +7,32 @@ describe Shutl::DynamicResource do
     include Shutl::DynamicResource
   end
 
+  let(:resource) { TestResource.new({ a: 'a', b: 'b' }) }
+
   describe '#initalize' do
 
     it 'should create a instance variable for the entries in the hash' do
-      resource = TestResource.new({ a: 'a', b: 'b' })
-
       resource.instance_variables.should =~ [ :@a, :@b ]
     end
 
     it 'should assign the instance variable value for the entries in the hash' do
-      resource = TestResource.new( { a: 'a', b: 'b' })
-
       resource.instance_variable_get(:@a).should == 'a'
       resource.instance_variable_get(:@b).should == 'b'
+    end
+  end
+
+  describe '#to_json' do
+
+    it 'should "prefix" the json with the name of the class' do
+      json = resource.to_json
+
+      JSON.parse(json, symbolize_names: true)[:test_resource].should_not be_nil
+    end
+
+    it 'should serialize in json based on the instance variables' do
+      json = resource.to_json
+
+      JSON.parse(json, symbolize_names: true).should == { test_resource: { a: 'a', b: 'b' } } 
     end
   end
 end
