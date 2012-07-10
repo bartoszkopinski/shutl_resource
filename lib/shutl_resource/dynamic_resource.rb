93b383e9e74de266b687630dcce53c11726c8b31
David Rouchy <drouchy@gmail.com>
2012-07-10 14:22:25 +0100

    move the resource_name to dynamic resource

--- lib/shutl_resource/dynamic_resource.rb
+++ lib/shutl_resource/dynamic_resource.rb
@@ -1,24 +1,32 @@
+require 'active_model/serialization'
+
 module Shutl
   module DynamicResource
+    include ActiveModel::Serialization
+
+    def self.included(base)
+      base.instance_variable_set :@resource_name, base.name.underscore
+    end
 
     def initialize(args = {})
       args.each { |key, value| instance_variable_set "@#{key}", value }
     end
 
-    def to_json
+    def to_json(options = nil)
       {
         :"#{prefix}" => to_hash
-      }.to_json
+      }.to_json(options)
     end
 
     protected
 
     def prefix
-      'test_resource'
+      self.class.instance_variable_get(:@resource_name)
     end
 
     def to_hash
-     instance_variables.inject({}) { |h, variable| h[variable.to_s.gsub('@','').to_sym] = instance_variable_get(variable) ; h }
+      instance_variables.inject({}) { |h, variable| h[variable.to_s.gsub('@','').to_sym] = instance_variable_get(variable) ; h }
     end
+
   end
 end
--- lib/shutl_resource/rest/rest_resource.rb
+++ lib/shutl_resource/rest/rest_resource.rb
@@ -11,53 +11,65 @@ module Shutl
       def self.included(base)
         base.send :include, HTTParty
         base.send :include, Shutl::DynamicResource
-        base.send :extend, ClassMethods
+        base.send :extend,  ClassMethods
         base.send :headers, { 'Content-Type' => 'application/json' } 
 
-        base.instance_variable_set :@remote_resource_name, base.name.underscore
+        base.send(:resource_id, :id)
       end
 
       module ClassMethods
 
         def find(id)
-          url = "/#{@remote_resource_name.pluralize}/#{id}"
+          url = "/#{@resource_name.pluralize}/#{id}"
           response = get(url)
 
           raise Shutl::RemoteError.new("Failed to find #{name} with the id #{id}") unless response.success?
 
-          new JSON.parse(response.body)[@remote_resource_name]
+          create_object JSON.parse(response.body, symbolize_names: true)[@resource_name.to_sym]
         end
 
         def all
-          url = "/#{@remote_resource_name.pluralize}"
+          url = "/#{@resource_name.pluralize}"
           response = get(url)
 
           raise Shutl::RemoteError.new("Failed to find all #{name}") unless response.success?
 
-          JSON.parse(response.body)[@remote_resource_name.pluralize].map { |h| new(h) }
+          JSON.parse(response.body, symbolize_names: true)[@resource_name.pluralize.to_sym].map { |h| create_object(h) }
         end
 
         def resource_id(variable_name)
           instance_variable_set :@resource_id, variable_name
         end
+
+
+        def create_object(args = {})
+          unless args.has_key? :id
+            args.merge!( { id: args[resource_id_name] })
+          end
+          new args
+        end
+
+        def resource_id_name
+          instance_variable_get(:@resource_id).to_sym
+        end
       end
 
       def create
-        url = "/#{self.class.instance_variable_get(:@remote_resource_name).pluralize}"
+        url = "/#{self.class.instance_variable_get(:@resource_name).pluralize}"
         response = self.class.post(url, body: to_json )
 
         response.success?
       end
 
       def delete
-        url = "/#{self.class.instance_variable_get(:@remote_resource_name).pluralize}/#{resource_id}"
+        url = "/#{self.class.instance_variable_get(:@resource_name).pluralize}/#{resource_id}"
         response = self.class.delete(url)
 
         response.success?
       end
 
       def save
-        url = "/#{self.class.instance_variable_get(:@remote_resource_name).pluralize}/#{resource_id}"
+        url = "/#{self.class.instance_variable_get(:@resource_name).pluralize}/#{resource_id}"
         response = self.class.put(url, body: to_json)
 
         response.success?
@@ -75,12 +87,10 @@ module Shutl
         raise Shutl::RemoteError.new("Failed to update the #{self.class.name}") unless updated
       end
 
-      private
-
       def resource_id
-        resource_name = self.class.instance_variable_get(:@resource_id)
-        instance_variable_get "@#{resource_name}"
+        instance_variable_get :"@#{self.class.resource_id_name}"
       end
+
     end
   end
 end
