f637bb63713697895a6b623a390cc66dd16628d8
David Rouchy <drouchy@gmail.com>
2013-11-13 15:27:41 +0000

    remove a warning on redefining constants

--- lib/shutl/resource/errors.rb
+++ lib/shutl/resource/errors.rb
@@ -10,16 +10,15 @@ end
 # The correct solution to this would be to remove this exception from the gem
 # and handle specifically in a QuoteCollection resource in for example the
 # 'shutl' gem.
-Shutl::NoQuotesGenerated  = Class.new Shutl::Resource::Error
-
-Shutl::BadRequest         = Class.new Shutl::Resource::Error
-Shutl::UnauthorizedAccess = Class.new Shutl::Resource::Error
-Shutl::ForbiddenAccess    = Class.new Shutl::Resource::Error
-Shutl::ResourceNotFound   = Class.new Shutl::Resource::Error
-Shutl::ResourceConflict   = Class.new Shutl::Resource::Error
-Shutl::ResourceGone       = Class.new Shutl::Resource::Error
-Shutl::ResourceInvalid    = Class.new Shutl::Resource::Error
-Shutl::ServerError        = Class.new Shutl::Resource::Error
-Shutl::ServiceUnavailable = Class.new Shutl::Resource::Error
-
-
+unless Shutl.const_defined?('NoQuotesGenerated')
+  Shutl::NoQuotesGenerated  = Class.new Shutl::Resource::Error
+  Shutl::BadRequest         = Class.new Shutl::Resource::Error
+  Shutl::UnauthorizedAccess = Class.new Shutl::Resource::Error
+  Shutl::ForbiddenAccess    = Class.new Shutl::Resource::Error
+  Shutl::ResourceNotFound   = Class.new Shutl::Resource::Error
+  Shutl::ResourceConflict   = Class.new Shutl::Resource::Error
+  Shutl::ResourceGone       = Class.new Shutl::Resource::Error
+  Shutl::ResourceInvalid    = Class.new Shutl::Resource::Error
+  Shutl::ServerError        = Class.new Shutl::Resource::Error
+  Shutl::ServiceUnavailable = Class.new Shutl::Resource::Error
+end
