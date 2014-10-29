9889c23918b201c3e58a012a2c801fd698d1f5ef
Volker Pacher <vpacher@ebay.com>
2014-10-29 14:42:59 +0000

    config spec

--- spec/configuration_spec.rb
+++ spec/configuration_spec.rb
@@ -14,5 +14,18 @@ describe Shutl::Resource do
         config.base_uri = nil
       end
     end
+
+    it "allows for configuration of the proxy uri" do
+      Shutl::Resource.configure do |config|
+        config.proxy_uri = 'proxy uri'
+      end
+
+      Shutl::Resource.base_uri.should == 'proxy uri'
+
+      #set it back to not corrupt other tests
+      Shutl::Resource.configure do |config|
+        config.proxy_uri = nil
+      end
+    end
   end
 end
